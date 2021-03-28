bc739d2e851d8df73a59e99b7decee2466b8321f
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-03-28 18:16:27 +0200

    Add totally serious command

---
+++ commands/komedi.js
@@ -0,0 +1,4 @@
+exports.run = (client, message) => {
+    message.delete();
+    message.channel.send('https://cdn.discordapp.com/attachments/768117219812835378/818145599894847488/eqmmb89gml941.png');
+}
