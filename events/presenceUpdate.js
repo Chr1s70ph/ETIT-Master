506ce48e9b5693585185112d62f908295617da5a
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-04-04 19:01:58 +0200

    Add a try catch to presence update

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -8,36 +8,42 @@ exports.run = async (client, oldPresence, newPresence) => {
     let member = newPresence.member;
     //UserID to track
     if (member.id === private.eTITChef) {
-        
-        if (oldPresence.status !== newPresence.status) {
 
-            //creates emergency Embed in case ETIT-Chef is offline
-            const emergency = new discord.MessageEmbed()
-                .setColor('#8B0000')
-                .setTitle('Der ETIT-Chef ist Offline!!')
-                .setAuthor('Offline Detector', client.guilds.resolve(private.serverId).members.resolve(private.botUserID).user.avatarURL())
-                .setThumbnail('https://wiki.jat-online.de/lib/exe/fetch.php?cache=&media=emoticons:lupe.png')
-                .addFields(
-                    { name: 'Leonard eile zur Hilfe!', value: 'Wir brauchen dich!' }
+        try {
+            if (oldPresence.status !== newPresence.status) {
+    
+                //creates emergency Embed in case ETIT-Chef is offline
+                const emergency = new discord.MessageEmbed()
+                    .setColor('#8B0000')
+                    .setTitle('Der ETIT-Chef ist Offline!!')
+                    .setAuthor('Offline Detector', client.guilds.resolve(private.serverId).members.resolve(private.botUserID).user.avatarURL())
+                    .setThumbnail('https://wiki.jat-online.de/lib/exe/fetch.php?cache=&media=emoticons:lupe.png')
+                    .addFields(
+                        { name: 'Leonard eile zur Hilfe!', value: 'Wir brauchen dich!' }
+                        
+                    )
+    
+                let channel = client.channels.cache.get(private.botTestLobby)
+                var leonard = private.leonard;
+                var eTITChef = private.eTITChef;
+    
+                if (newPresence.status === "offline") {
                     
-                )
-
-            let channel = client.channels.cache.get(private.botTestLobby)
-            var leonard = private.leonard;
-            var eTITChef = private.eTITChef;
-
-            if (newPresence.status === "offline") {
-                
-                channel.send('<@' + private.leonard + '>', emergency);
-                // channel.send('<@' + private.leonard + '>', emergency);
-
-            } else if (newPresence.status === "online") {
-
-                return;
-
-            }
-
-        }      
+                    channel.send('<@' + private.leonard + '>', emergency);
+                    // channel.send('<@' + private.leonard + '>', emergency);
+    
+                } else if (newPresence.status === "online") {
+    
+                    return;
+    
+                }
+    
+            }      
+            
+        } catch (e) {
+            console.log(e);
+        }
+        
     }
 }
 
