6b38d0031c04668ae2b62358e8e7e867f787a140
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-05-01 01:19:07 +0200

    add info to magic number

--- slashCommands/slashHelp.js
+++ slashCommands/slashHelp.js
@@ -16,7 +16,7 @@ async function help(client) {
         if (command === 'help'){
             await client.api.interactions(interaction.id, interaction.token).callback.post({
                 data: {
-                    type: 4,
+                    type: 4, //https://discord.com/developers/docs/interactions/slash-commands#interaction-response-interactionresponsetype
                     data: {
                         content: "```asciidoc\n = Die Hauptfunktion des Bots ist es, an Vorlesungen und in Zukunft auch an Tutorien zu erinnern =\n----------" +
                             "\nVorlesungserinnerung :: Der Bot schickt automatisch in den Channel des jeweiligen Faches 5 Minuten vor Vorlesungsbeginn einen Link zum jeweiligen Zoom-Meeting (oder zur YouTube Playlist bei Experimental Physik.\n```",
