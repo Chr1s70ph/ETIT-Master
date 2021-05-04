bcfaf18f7bca144759780519aef9f89d06d992c7
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-05-04 12:33:18 +0200

    Add Emoji to member counter

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,6 +5,6 @@ exports.run = async (client, message) => {
     const guild = client.guilds.cache.get(config.ids.serverID);
     const memberCount = guild.memberCount;
     const channel = guild.channels.cache.get(config.ids.channelIDs.dev.memberCounter);
-    channel.setName(`:busts_in_silhouette: Mitglieder:${memberCount.toLocaleString()}`);
+    channel.setName(`👥 Mitglieder:${memberCount.toLocaleString()}`);
     console.log(`Updated membercount to ${memberCount.toLocaleString()}`)
 }
