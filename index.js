7ed00873158fb2c8ede3882467cd827e2fbe357d
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-05-12 00:03:07 +0200

    complete rework of how commands are added

--- index.js
+++ index.js
@@ -29,20 +29,43 @@ client.login(config.botToken);
 /**
  * 
  */
-fs.readdir("./commands/", (err, files) => {
+fs.readdir("./commands/", (err, elements) => {
+    var path = "./commands/";
     if (err) return console.log(err);
-    files.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        let props = require(`./commands/${file}`);
-        console.log("Successfully loaded command " + file)
-        let commandName = file.split(".")[0];
-        client.commands.set(commandName, props);
+    elements.forEach(file => { //loop through all elements in folder "commands"
+        const element_in_folder = fs.statSync(`./commands/${file}`)
+        if (element_in_folder.isDirectory() == true) { //check if element in folder is a subfolder
+            const sub_directory = `./commands/${file}/`;
+            fs.readdir(sub_directory, (err, files) => {
+                if (err) return console.log(err);
+                files.forEach(file => {
+                    setCommands(sub_directory, file, client); //adds commands from subfolder to collection
+                });
+            });
+            return; 
+        }
+        
+        setCommands(path, file, client) //adds commands from parentfolder to collection
     });
 });
 
+/**
+ * Adds all commands to the commands collection
+ * @param {string} path relative path to the file of each command
+ * @param {string} file actual filename of the file
+ * @param {Object} client 
+ */
+function setCommands(path, file, client) {
+    if (!file.endsWith(".js")) return;
+    let props = require(`${path}${file}`);
+    console.log("Successfully loaded command " + file)
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+}
+
 //Events "handler"
 /**
- * 
+ *  loads all events and adds them to the event collection
  */
 fs.readdir('./events/', (err, files) => {
     if (err) console.log(err);
