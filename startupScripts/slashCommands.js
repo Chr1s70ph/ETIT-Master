33627f938a77ad8b95e8e9d449650ad93fc62058
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-05-25 12:35:27 +0200

    remove slashBirthday

--- startupScripts/slashCommands.js
+++ startupScripts/slashCommands.js
@@ -1,21 +1,22 @@
 let help = require("../slashCommands/slashHelp.js")
 let ping = require("../slashCommands/slashPing.js")
 let info = require("../slashCommands/slashInfo.js");
-let birthday = require("../slashCommands/slashBirthday.js");
+let config = require('../privateData/config.json');
 
 exports.run = async (client) => {
     await help.help(client);
     await ping.Ping(client);
     await info.info(client);
-    await birthday.birthdayEntry(client);
 }
 
 
 // Code to get all posted Interactions, log them, and delete them (by id)
 // async function GetPostsDelete(client) {
-//     const interactions = await client.api.applications(client.user.id).guilds(serverID).commands.get();
+    // const interactions = await client.api.applications(client.user.id).guilds(config.ids.serverID).commands.get();
+    // const interactions = await client.api.applications(client.user.id).commands.get();
 
-//     console.log(interactions);
-//     client.api.applications(client.user.id).guilds(serverID).commands('777313468377858059').delete();
+    // console.log(interactions);
+    // client.api.applications(client.user.id).guilds(config.ids.serverID).commands('idHERE').delete();
+    // client.api.applications(client.user.id).commands('idHERE').delete();
 //     console.log(interactions);
 // }
