c868f9812c004e3f8f9e5dfc674848e468fb55d7
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-05-27 01:52:25 +0200

    Rework dev commands

--- commands/dev/restart.js
+++ commands/dev/restart.js
@@ -2,22 +2,19 @@ const pm2 = require('pm2');
 const config = require("../../privateData/config.json");
 
 exports.run = (client, message) => {
+    if (message.author != config.ids.userID.ownerID) return message.reply('You do not have the permissions to perform that command.');
 
-    if (message.author == config.ids.userID.ownerID) {
-        message.channel.send("🤖Restarting...")
-        pm2.connect(function (err) {
-            if (err) {
-                console.error(err);
-                process.exit(2);
-            }
+    message.channel.send("🤖Restarting...")
+    pm2.connect(function (err) {
+        if (err) {
+            console.error(err);
+            process.exit(2);
+        }
 
-            pm2.restart('index', (err, proc) => {})
-            pm2.flush('index', (err, proc) => {})
-            pm2.restart('index', (err, proc) => {})
+        pm2.restart('index', (err, proc) => {})
+        pm2.flush('index', (err, proc) => {})
+        pm2.restart('index', (err, proc) => {})
 
-        });
+    });
 
-    } else {
-        message.reply('You do not have the permissions to perform that command.');
-    }
 }
