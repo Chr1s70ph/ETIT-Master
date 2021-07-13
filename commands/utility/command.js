a0fec15643b58436dbb0eb45bea343b18a5b0970
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-07-13 10:45:51 +0200

    Add aliases to command Helper Embed

--- commands/utility/command.js
+++ commands/utility/command.js
@@ -11,7 +11,13 @@ exports.example = `${config.prefix}command test`;
 
 exports.aliases = ['commandinfo'];
 
-
+/**
+ * 
+ * @param {object} client discord bot client
+ * @param {object} message message object
+ * @param {array} args arguments of issued command
+ * @returns commandHelpEmbed with description, example and name of command
+ */
 exports.run = (client, message, args) => {
     let commandHelpEmbed = new discord.MessageEmbed()
         .setColor('#7289da')
@@ -19,14 +25,15 @@ exports.run = (client, message, args) => {
         .setThumbnail(client.guilds.resolve(config.ids.serverID).members.resolve(config.ids.userID.botUserID).user.avatarURL())
 
 
+    if (args[0] == undefined) {
+        return message.channel.send("Please provide arguments!")
+    }
+
 
     for (let [key, value] of client.commands.entries()) {
+        if (key == args[0].toLowerCase() || findAliases(value.aliases, args)) {
+            addAliasesToEmbed(value.aliases, commandHelpEmbed, args);
 
-        if (args[0] == undefined) {
-            return message.channel.send("Please provide arguments!")
-        }
-
-        if (key == args[0].toLowerCase()) {
             commandHelpEmbed.setTitle(`‎${value.name}\n ‎`)
             commandHelpEmbed.addFields({
                 name: 'Beschreibung',
@@ -38,9 +45,43 @@ exports.run = (client, message, args) => {
                 inline: false
             });
             return message.channel.send(commandHelpEmbed);
+
         }
+
     }
 
     return message.channel.send("Bitte verwende einen Commandnamen.")
 
+}
+
+/**
+ * returns aliases if command has defined aliases
+ * @param {array} aliasesArray array of aliases exported
+ * @param {array} args arguments of issued command
+ * @returns array of aliases
+ */
+function findAliases(aliasesArray, args) {
+    if (Array.isArray(aliasesArray) && aliasesArray.length > 0) {
+        return aliasesArray.some(commandName => commandName.toLowerCase() === args[0].toLowerCase());
+    } else return false;
+}
+
+/**
+ * 
+ * @param {array} aliasesArray array of aliases exported
+ * @param {object} commandHelpEmbed command embed
+ * @param {array} args arguments of issued command
+ * @returns commandHelpEmbed with added aliases
+ */
+function addAliasesToEmbed(aliasesArray, commandHelpEmbed, args) {
+    if (findAliases(aliasesArray, args)) {
+        let aliasesString = aliasesArray.toString();
+        commandHelpEmbed.addFields({
+            name: 'Aliase',
+            value: `${aliasesString}\n `,
+            inline: false
+        });
+
+        return commandHelpEmbed;
+    }
 }
