5a4cd275892a38a229b660623284c54a3e8c310d
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-09-06 22:55:42 +0200

    send welcome message to new members

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,32 @@
 const config = require("../private/config.json")
+const discord = require("discord.js")
 
-exports.run = async (client, message) => {
+exports.run = async (client, member) => {
 	const guild = client.guilds.cache.get(config.ids.serverID)
 	const memberCount = guild.memberCount
 	const channel = guild.channels.cache.get(config.ids.channelIDs.dev.memberCounter)
 	channel.setName(`👥 ${memberCount.toLocaleString()} Mitglieder`)
-	console.log(`New Member joined. Updated membercount to ${memberCount.toLocaleString()}`)
+	console.log(
+		`${member.user.username} joined. Updated membercount to ${memberCount.toLocaleString()}`
+	)
+
+	let welcomeMessage = new discord.MessageEmbed()
+		.setTitle("🗲 Willkommen auf dem ETIT-KIT Server 🗲")
+		.setColor("#FFDA00")
+		.setAuthor(client.user.tag, member.guild.iconURL())
+		.setThumbnail(
+			client.guilds
+				.resolve(config.ids.serverID)
+				.members.resolve(config.ids.userID.botUserID)
+				.user.avatarURL()
+		)
+		.setDescription(`Wir hoffen, dass der Server dir gefällt, und dir im Studium weiterhelfen kann.
+		In <#830837597587767306> kannst du deinen Studiengang auswählen.
+		In der Kategorie <#830891013266604062> findest du dann weitere Kanäle, in denen du deine Fächer auswählen kannst.
+
+		Du darfst auch gerne alle deine Freunde einladen, wir heißen jeden Willkommen!
+		
+		Falls du noch irgendwelche Fragen hast, wende dich einfach an einen Admin des Servers (wir sind auch nur einfache Studenten, genauso wie du).`)
+
+	member.send({ embeds: [welcomeMessage] })
 }
