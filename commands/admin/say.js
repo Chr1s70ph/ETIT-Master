7a4722e2f7a0035e22651ca5c149c7014e775fee
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-09-16 18:18:03 +0200

    add reply and image forwarding to say command

--- commands/admin/say.js
+++ commands/admin/say.js
@@ -12,10 +12,22 @@ exports.run = async (client, message) => {
 	if (!Object.values(config.ids.acceptedAdmins).includes(message.author.id))
 		return message.reply("You do not have the permissions to perform that command.")
 
+	let messageContent = message.content.substring(
+		message.content.indexOf(" ") + config.prefix.length
+	)
+
 	const embed = new discord.MessageEmbed()
+		.setDescription(messageContent === `${config.prefix}say` ? "᲼" : messageContent)
+		.setColor("RANDOM")
 
-	var messageContent = message.content.substring(message.content.indexOf(" ") + 1)
+	let messageAttachment =
+		message.attachments.size > 0 ? message.attachments.first().url : null
+	embed.setImage(messageAttachment)
 
-	embed.setDescription(messageContent)
+	if (message.type === "REPLY") {
+		return (message = message.channel.messages
+			.fetch(message.reference.messageId)
+			.then((message) => message.reply({ embeds: [embed] })))
+	}
 	message.channel.send({ embeds: [embed] })
 }
