fcb41215f5961b157b6e0c1798d514375c5928f3
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-09-16 18:18:49 +0200

    use reply and messageEmbed

--- commands/utility/test.js
+++ commands/utility/test.js
@@ -1,3 +1,5 @@
+const discord = require("discord.js")
+
 const config = require("../../private/config.json")
 
 exports.name = "test"
@@ -7,5 +9,7 @@ exports.description = "Prüft ob der Bot online und funktionstüchtig ist."
 exports.usage = `${config.prefix}test`
 
 exports.run = async (client, message) => {
-	message.channel.send("🌐 This Bot is working as intended!")
+	message.reply({
+		embeds: [new discord.MessageEmbed().setDescription("🌐 This Bot is working as intended!")]
+	})
 }
