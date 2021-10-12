e6f034568e11566cdc838ddf3e468e17e272e639
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-12 18:32:36 +0200

    completely rework uptime command

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -1,4 +1,5 @@
 const config = require("../../private/config.json")
+const discord = require("discord.js")
 
 exports.name = "uptime"
 
@@ -7,8 +8,40 @@ exports.description = "Wie lange ist der Bot schon online"
 exports.usage = `${config.prefix}uptime`
 
 exports.run = async (client, message) => {
-	//fetch process uptime, and get rid of values, after decimal point
-	let uptimeSeconds = process.uptime().toString().split(".")[0]
-	//https://stackoverflow.com/a/1322771/10926046
-	return message.reply(`${new Date(uptimeSeconds * 1000).toISOString().substr(11, 8)}`)
+	let uptime = {
+		days: null,
+		hours: null,
+		minutes: null,
+		seconds: null
+	}
+
+	// https://stackoverflow.com/a/36099084/10926046
+	uptime.seconds = process.uptime().toString().split(".")[0]
+
+	uptime.days = Math.floor(uptime.seconds / (3600 * 24))
+	uptime.seconds -= uptime.days * 3600 * 24
+	uptime.hours = Math.floor(uptime.seconds / 3600)
+	uptime.seconds -= uptime.hours * 3600
+	uptime.minutes = Math.floor(uptime.seconds / 60)
+	uptime.seconds -= uptime.minutes * 60
+
+	console.log(uptime)
+	return message.reply({
+		embeds: [
+			new discord.MessageEmbed()
+				.setTitle("⏰Uptime")
+				.addField(
+					"Time since last restart:",
+					uptime.days +
+						" Days, " +
+						uptime.hours +
+						" Hours, " +
+						uptime.minutes +
+						" Minutes, " +
+						uptime.seconds +
+						" Seconds"
+				)
+				.setColor("#FF4040")
+		]
+	})
 }
