c9e5aa708fa38806a327b89fbdcb280b2a04ad8f
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-12 18:31:26 +0200

    rework countdown command

--- commands/utility/countdown.js
+++ commands/utility/countdown.js
@@ -1,4 +1,5 @@
 const config = require("../../private/config.json")
+const discord = require("discord.js")
 
 exports.name = "countdown"
 
@@ -7,21 +8,29 @@ exports.description = "Ein simpler countdown von 10 runter"
 exports.usage = `${config.prefix}countdown`
 
 exports.run = async (client, message) => {
-	message.channel.send("Liftoff in:")
-	message.channel.send("🚀10🚀")
-	setTimeout(() => {
-		message.channel.send("🚀5🚀")
-	}, 5000)
-	setTimeout(() => {
-		message.channel.send("🚀3🚀")
-	}, 7000)
-	setTimeout(() => {
-		message.channel.send("🚀2🚀")
-	}, 8000)
-	setTimeout(() => {
-		message.channel.send("🚀1🚀")
-	}, 9000)
-	setTimeout(() => {
-		message.channel.send("Hurraaa 🚀🚀")
-	}, 10000)
+	let msgEmbed = new discord.MessageEmbed()
+		.setTitle("Liftoff in:")
+		.setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
+		.setThumbnail(
+			"https://upload.wikimedia.org/wikipedia/commons/3/33/Cartoon_space_rocket.png"
+		)
+	let msg = await message.reply({
+		embeds: [msgEmbed.setDescription(`🚀10🚀`)]
+	})
+	// counts only 8, 6, 4, 3, 2, 1, 0 and skips 9, 7, 5 due to API limits
+	for (let i = 9; i >= 0; i--) {
+		if (i > 3) {
+			if (i % 2 == 0) {
+				setTimeout(() => {
+					msg.edit({ embeds: [msgEmbed.setDescription(`🚀${i}🚀`)] })
+				}, 1000 * (10 - i))
+			}
+		} else {
+			setTimeout(() => {
+				if (i == 0)
+					msg.edit({ embeds: [msgEmbed.setTitle("Abfluuug").setDescription(`🚀Abgehoben🚀`)] })
+				else msg.edit({ embeds: [msgEmbed.setDescription(`🚀${i}🚀`)] })
+			}, 1000 * (10 - i))
+		}
+	}
 }
