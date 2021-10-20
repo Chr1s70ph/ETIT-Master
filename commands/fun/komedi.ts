86f32847b0562ab63e376ea6999daa6844d1d9d3
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-20 22:30:30 +0200

    port komedi

--- commands/fun/komedi.ts
+++ commands/fun/komedi.ts
@@ -1,16 +1,16 @@
-const config = require("../../private/config.json")
-const discord = require("discord.js")
+import { MessageEmbed, Message } from "discord.js"
+import { DiscordClient } from "../../index"
 
 exports.name = "komedi"
 
 exports.description = "Das Komedi Meme"
 
-exports.usage = `${config.prefix}komedi`
+exports.usage = "komedi"
 
-exports.run = async (client, message) => {
+exports.run = async (client: DiscordClient, message: Message) => {
 	return message.reply({
 		embeds: [
-			new discord.MessageEmbed()
+			new MessageEmbed()
 				.setImage(
 					"https://cdn.discordapp.com/attachments/768117219812835378/818145599894847488/eqmmb89gml941.png"
 				)
