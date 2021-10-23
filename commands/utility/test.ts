88bdf09b8ed390259faf08c4f602c8d04f186478
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-23 15:04:45 +0200

    port test

--- commands/utility/test.ts
+++ commands/utility/test.ts
@@ -1,16 +1,16 @@
-const discord = require("discord.js")
-const config = require("../../private/config.json")
+import { DiscordClient } from "../../index"
+import { Message, MessageEmbed } from "discord.js"
 
 exports.name = "test"
 
 exports.description = "Prüft ob der Bot online und funktionstüchtig ist."
 
-exports.usage = `${config.prefix}test`
+exports.usage = "test"
 
-exports.run = async (client, message) => {
+exports.run = async (client: DiscordClient, message: Message) => {
 	message.reply({
 		embeds: [
-			new discord.MessageEmbed()
+			new MessageEmbed()
 				.setTitle("🌐 This Bot is working as intended!")
 				.setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
 		]
