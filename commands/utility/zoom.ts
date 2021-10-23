7998345434ef08e705c997df6bde5e8521344c6e
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-23 15:04:59 +0200

    port zoom

--- commands/utility/zoom.ts
+++ commands/utility/zoom.ts
@@ -1,13 +1,13 @@
-const config = require("../../private/config.json")
-
+import { DiscordClient } from "../../index"
+import { Message } from "discord.js"
 exports.name = "zoom"
 
 exports.description = "Schickt ein Emoji, das beschreibt, worin Zoom am besten ist..."
 
-exports.usage = `${config.prefix}zoom`
+exports.usage = "zoom"
 
 exports.aliases = ["loading"]
 
-exports.run = async (client, message) => {
+exports.run = async (client: DiscordClient, message: Message) => {
 	message.channel.send("<a:loading:783688114509578271>")
 }
