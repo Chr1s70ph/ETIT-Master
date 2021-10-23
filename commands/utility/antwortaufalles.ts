f5701bad34803da231215445102988f1990d6a65
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-23 15:03:40 +0200

    port antwortaufalles

--- commands/utility/antwortaufalles.ts
+++ commands/utility/antwortaufalles.ts
@@ -1,16 +1,16 @@
-const config = require("../../private/config.json")
-const discord = require("discord.js")
+import { DiscordClient } from "../../index"
+import { Message, MessageEmbed } from "discord.js"
 
 exports.name = "antwortaufalles"
 
 exports.description = "Was ist die Antwort auf alles?"
 
-exports.usage = `${config.prefix}antwortaufalles`
+exports.usage = "antwortaufalles"
 
-exports.run = async (client, message) => {
+exports.run = async (client: DiscordClient, message: Message) => {
 	return message.reply({
 		embeds: [
-			new discord.MessageEmbed().setDescription(
+			new MessageEmbed().setDescription(
 				"Die Antwort auf die Frage nach dem Leben, dem Universum und dem ganzen Rest ist :four::two:"
 			)
 		]
