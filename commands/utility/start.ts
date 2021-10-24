3ed72068687db915c0b3b837d640dc6f9b1a5ff1
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-24 07:21:22 +0200

    update strings and import fetch

--- commands/utility/start.ts
+++ commands/utility/start.ts
@@ -1,5 +1,6 @@
 import { DiscordClient } from "../../index"
 import { Message, MessageEmbed } from "discord.js"
+import fetch from "node-fetch"
 /**
  * NOTE:
  * This code is heavily inspired by the discord-together package
@@ -8,18 +9,18 @@ import { Message, MessageEmbed } from "discord.js"
  */
 
 const defaultApplications = {
-	youtube: '880218394199220334', // Note : First package to include the new YouTube Together version, any other package offering it will be clearly inspired by it
-	youtubedev: '880218832743055411', // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
-	poker: '755827207812677713',
-	betrayal: '773336526917861400',
-	fishing: '814288819477020702',
-	chess: '832012774040141894',
-	chessdev: '832012586023256104', // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
-	lettertile: '879863686565621790', // Note : First package to offer lettertile, any other package offering it will be clearly inspired by it
-	wordsnack: '879863976006127627', // Note : First package to offer wordsnack any other package offering it will be clearly inspired by it
-	doodlecrew: '878067389634314250', // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
-	awkword: '879863881349087252', // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
-	spellcast: '852509694341283871', // Note : First package to offer spellcast, any other package offering it will be clearly inspired by it
+	youtube: "880218394199220334", // Note : First package to include the new YouTube Together version, any other package offering it will be clearly inspired by it
+	youtubedev: "880218832743055411", // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
+	poker: "755827207812677713",
+	betrayal: "773336526917861400",
+	fishing: "814288819477020702",
+	chess: "832012774040141894",
+	chessdev: "832012586023256104", // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
+	lettertile: "879863686565621790", // Note : First package to offer lettertile, any other package offering it will be clearly inspired by it
+	wordsnack: "879863976006127627", // Note : First package to offer wordsnack any other package offering it will be clearly inspired by it
+	doodlecrew: "878067389634314250", // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
+	awkword: "879863881349087252", // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
+	spellcast: "852509694341283871" // Note : First package to offer spellcast, any other package offering it will be clearly inspired by it
 }
 
 exports.name = "start"
