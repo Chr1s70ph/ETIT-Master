ed195d203ea3c453f701355833d67ea9a19a7166
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-10-27 22:58:09 +0200

    add protection against double events

--- scripts/iCalReader.ts
+++ scripts/iCalReader.ts
@@ -190,108 +190,108 @@ function getEvents(data: {}, today: Date, events: {}, client: DiscordClient) {
 	todayEnd.setSeconds(59)
 
 	for (var k in data) {
-		// When dealing with calendar recurrences, you need a range of dates to query against,
-		// because otherwise you can get an infinite number of calendar events.
-		var rangeStart = moment(today)
-		var rangeEnd = moment(todayEnd)
+		if (data.hasOwnProperty(k)) {
+			// When dealing with calendar recurrences, you need a range of dates to query against,
+			// because otherwise you can get an infinite number of calendar events.
+			var rangeStart = moment(today)
+			var rangeEnd = moment(todayEnd)
 
-		var event = data[k]
-		if (event.type === "VEVENT") {
-			var title = event.summary
-			var description = event.description
-			var startDate = moment(event.start)
-			var endDate = moment(event.end)
+			var event = data[k]
+			if (event.type === "VEVENT") {
+				var title = event.summary
+				var description = event.description
+				var startDate = moment(event.start)
+				var endDate = moment(event.end)
 
-			// Calculate the duration of the event for use with recurring events.
-			var duration = parseInt(endDate.format("x")) - parseInt(startDate.format("x"))
+				// Calculate the duration of the event for use with recurring events.
+				var duration =
+					Number.parseInt(endDate.format("x"), 10) - Number.parseInt(startDate.format("x"), 10)
 
-			// Simple case - no recurrences, just print out the calendar event.
-			if (typeof event.rrule === "undefined" && datesAreOnSameDay(event.start, today)) {
-				addEntryToWeeksEvents(
-					events,
-					today.getDay().toString(),
-					event.start,
-					title,
-					description,
-					event.location
-				)
-			}
-
-			// Complicated case - if an RRULE exists, handle multiple recurrences of the event.
-			else if (typeof event.rrule !== "undefined") {
-				// For recurring events, get the set of event start dates that fall within the range
-				// of dates we're looking for.
-				var dates = event.rrule.between(
-					rangeStart.toDate(),
-					rangeEnd.toDate(),
-					true,
-					function (date, i) {
+				// Simple case - no recurrences, just print out the calendar event.
+				if (typeof event.rrule === "undefined" && datesAreOnSameDay(event.start, today)) {
+					addEntryToWeeksEvents(
+						events,
+						today.getDay().toString(),
+						event.start,
+						title,
+						description,
+						event.location
+					)
+				} else if (typeof event.rrule !== "undefined") {
+					// Complicated case - if an RRULE exists, handle multiple recurrences of the event.
+					// For recurring events, get the set of event start dates that fall within the range
+					// of dates we're looking for.
+					const dates = event.rrule.between(rangeStart.toDate(), rangeEnd.toDate(), true, () => {
 						return true
-					}
-				)
+					})
 
-				// The "dates" array contains the set of dates within our desired date range range that are valid
-				// for the recurrence rule.  *However*, it's possible for us to have a specific recurrence that
-				// had its date changed from outside the range to inside the range.  One way to handle this is
-				// to add *all* recurrence override entries into the set of dates that we check, and then later
-				// filter out any recurrences that don't actually belong within our range.
-				if (event.recurrences != undefined) {
-					for (var r in event.recurrences) {
-						// Only add dates that weren't already in the range we added from the rrule so that
-						// we don't double-add those events.
-						if (moment(new Date(r)).isBetween(rangeStart, rangeEnd) != true) {
-							dates.push(new Date(r))
+					// The "dates" array contains the set of dates within our desired date range range that are valid
+					// for the recurrence rule.  *However*, it's possible for us to have a specific recurrence that
+					// had its date changed from outside the range to inside the range.  One way to handle this is
+					// to add *all* recurrence override entries into the set of dates that we check, and then later
+					// filter out any recurrences that don't actually belong within our range.
+					if (event.recurrences !== undefined) {
+						for (const r in event.recurrences) {
+							// Only add dates that weren't already in the range we added from the rrule so that
+							// we don't double-add those events.
+							if (moment(new Date(r)).isBetween(rangeStart, rangeEnd) !== true) {
+								dates.push(new Date(r))
+							}
 						}
 					}
-				}
 
-				// Loop through the set of date entries to see which recurrences should be printed.
-				for (var i in dates) {
-					var date = dates[i]
-					var curEvent = event
-					var showRecurrence = true
-					var curDuration = duration
+					// Loop through the set of date entries to see which recurrences should be printed.
+					for (const i in dates) {
+						const date = dates[i]
+						let curEvent = event
+						let showRecurrence = true
+						let curDuration = duration
 
-					startDate = moment(date)
+						startDate = moment(date)
 
-					// Use just the date of the recurrence to look up overrides and exceptions (i.e. chop off time information)
-					var dateLookupKey = date.toISOString().substring(0, 10)
+						// Use just the date of the recurrence to look up overrides and exceptions (i.e. chop off time information)
+						const dateLookupKey = date.toISOString().slice(0, 10)
 
-					// For each date that we're checking, it's possible that there is a recurrence override for that one day.
-					if (
-						curEvent.recurrences != undefined &&
-						curEvent.recurrences[dateLookupKey] != undefined
-					) {
-						// We found an override, so for this recurrence, use a potentially different title, start date, and duration.
-						curEvent = curEvent.recurrences[dateLookupKey]
-						startDate = moment(curEvent.start)
-						curDuration = parseInt(moment(curEvent.end).format("x")) - parseInt(startDate.format("x"))
-					}
-					// If there's no recurrence override, check for an exception date.  Exception dates represent exceptions to the rule.
-					else if (curEvent.exdate != undefined && curEvent.exdate[dateLookupKey] != undefined) {
-						// This date is an exception date, which means we should skip it in the recurrence pattern.
-						showRecurrence = false
-					}
+						// For each date that we're checking, it's possible that there is a recurrence override for that one day.
+						if (
+							curEvent.recurrences !== undefined &&
+							curEvent.recurrences[dateLookupKey] !== undefined
+						) {
+							// We found an override, so for this recurrence, use a potentially different title, start date, and duration.
+							curEvent = curEvent.recurrences[dateLookupKey]
+							startDate = moment(curEvent.start)
+							curDuration =
+								Number.parseInt(moment(curEvent.end).format("x"), 10) -
+								Number.parseInt(startDate.format("x"), 10)
+						} else if (
+							curEvent.exdate !== undefined &&
+							curEvent.exdate[dateLookupKey] !== undefined
+						) {
+							// If there's no recurrence override, check for an exception date.  Exception dates represent exceptions to the rule.
+							// This date is an exception date, which means we should skip it in the recurrence pattern.
+							showRecurrence = false
+						}
 
-					// Set the the title and the end date from either the regular event or the recurrence override.
-					var recurrenceTitle = curEvent.summary
-					endDate = moment(parseInt(startDate.format("x")) + curDuration, "x")
+						// Set the the title and the end date from either the regular event or the recurrence override.
+						const recurrenceTitle = curEvent.summary
+						endDate = moment(Number.parseInt(startDate.format("x"), 10) + curDuration, "x")
 
-					// If this recurrence ends before the start of the date range, or starts after the end of the date range,
-					// don't process it.
-					if (endDate.isBefore(rangeStart) || startDate.isAfter(rangeEnd)) {
-						showRecurrence = false
-					}
+						// If this recurrence ends before the start of the date range, or starts after the end of the date range,
+						// don't process it.
+						if (endDate.isBefore(rangeStart) || startDate.isAfter(rangeEnd)) {
+							showRecurrence = false
+						}
 
-					if (showRecurrence === true) {
-						addEntryToWeeksEvents(
-							events,
-							today.getDay().toString(),
-							event.start,
-							title,
-							description,
-							event.location
-						)
+						if (showRecurrence === true && datesAreOnSameDay(date, today)) {
+							addEntryToWeeksEvents(
+								events,
+								today.getDay().toString(),
+								event.start,
+								title,
+								description,
+								event.location
+							)
+						}
 					}
 				}
 			}
@@ -352,6 +352,17 @@ function addEntryToWeeksEvents(
 	description: string,
 	location: string
 ) {
+	//protection against double events
+	for (var elemtent in events) {
+		if (
+			events[elemtent].start === start &&
+			events[elemtent].summary === summary &&
+			events[elemtent].description === description &&
+			events[elemtent].location === location
+		) {
+			return events
+		}
+	}
 	events[Object.keys(events).length] = {
 		day: day,
 		start: start,
