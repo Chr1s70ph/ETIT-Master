eb83799bdfc29bf0b68c8061c79a75623c586f48
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-14 18:48:24 +0100

    add promise command-return functions

--- types/customTypes.ts
+++ types/customTypes.ts
@@ -1,4 +1,5 @@
-import { Client } from 'discord.js'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Client, Message, TextChannel, MessageOptions } from 'discord.js'
 
 export class DiscordClient extends Client {
   public commands
@@ -16,5 +17,37 @@ export class DiscordClient extends Client {
     doodlecrew: string
     awkword: string
     spellcast: string
+  }
+  /**
+   * Uses {@link TextChannel.send()} to reply to the issued command.
+   * @param {Message} message message to answer to
+   * @param {MessageOptions} returnData data to be sent back as answer
+   * @returns {Message} original command message
+   */
+  public commandSendPromise(message: Message, returnData: MessageOptions) {
+    return new Promise<Message>((resolve, reject) => {
+      try {
+        message.channel.send(returnData)
+        resolve(message)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  /**
+   * Uses {@link Message.reply()} to reply to the issued command.
+   * @param {Message} message message to ryply to
+   * @param {MessageOptions} returnData data to be sent back as answer
+   * @returns {Message} original command message
+   */
+  public commandReplyPromise(message: Message, returnData: MessageOptions) {
+    return new Promise<Message>((resolve, reject) => {
+      try {
+        message.reply(returnData)
+        resolve(message)
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 }
