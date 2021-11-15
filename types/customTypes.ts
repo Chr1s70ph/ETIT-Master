07b46412004f4b117eeea461be8a1ddfd511a1dc
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-15 07:37:12 +0100

    Add param `optionalReplyMessage`

--- types/customTypes.ts
+++ types/customTypes.ts
@@ -40,11 +40,13 @@ export class DiscordClient extends Client {
    * Uses {@link Message.reply()} to reply to the issued command.
    * @param {Message} message message to ryply to
    * @param {MessageOptions} returnData data to be sent back as answer
+   * @param {Message} optionalReplyMessage to reply to, instead of command message
    * @returns {Message} original command message
    */
-  public commandReplyPromise(message: Message, returnData: MessageOptions) {
+  public commandReplyPromise(message: Message, returnData: MessageOptions, optionalReplyMessage?: Message) {
+    const replyMessage = optionalReplyMessage ?? message
     return new Promise<Message>((resolve, reject) => {
-      message.reply(returnData).then(
+      replyMessage.reply(returnData).then(
         () => {
           resolve(message)
         },
