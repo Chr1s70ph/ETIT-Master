a655c921b97570137322d588f7387c9109605ea6
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-25 13:12:11 +0100

    refactor slashHelp

--- slashCommands/slashHelp.ts
+++ slashCommands/slashHelp.ts
@@ -1,79 +1,69 @@
+import { Interaction, MessageEmbed } from 'discord.js'
 const fs = require('fs')
-const discord = require('discord.js')
-const config = require('../private/config.json')
 
 exports.run = async client => {
-  const embed = await getCommands()
+  const embed = await getCommands(client)
+  await postSlashCommand(client)
 
+  client.ws.on('INTERACTION_CREATE', async (interaction: Interaction) => {
+    if (!interaction.isCommand()) return
+    const COMMAND = interaction.commandName
+    if (COMMAND !== 'help') return
+    await respond(interaction, embed)
+  })
+}
+
+async function postSlashCommand(client: any): Promise<void> {
   await client.api.applications(client.user.id).commands.post({
     data: {
       name: 'help',
       description: 'hilfe ist hier',
     },
   })
+}
 
-  client.ws.on('INTERACTION_CREATE', async interaction => {
-    // Type 2 interactions are slashcommands
-    if (interaction.type !== '2') return
-    const command = interaction.data.name.toLowerCase()
-    if (command === 'help') {
-      console.log(`User ${interaction.member.user.username} issued /help`)
+async function respond(interaction: any, embed: any): Promise<void> {
+  console.log(`User ${interaction.user.username} issued /help`)
+  await interaction.reply({ embeds: [embed.setTimestamp()], ephemeral: true })
+}
 
-      await client.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-          // Discord.com/developers/docs/interactions/slash-commands#interaction-response-interactionresponsetype
-          type: 4,
-          data: {
-            embeds: [embed.setTimestamp()],
-            flags: 64,
-          },
-        },
-      })
+async function getCommands(client): Promise<any> {
+  const commandsEmbed = new MessageEmbed()
+    .setColor('#ffa500')
+    .setAuthor({ name: 'Help', iconURL: 'https://bit.ly/3CJU0lf' })
+    .setTimestamp()
+    .setFooter(
+      `[ID] ${client.config.ids.userID.botUserID} \nstarted`,
+      'https://image.flaticon.com/icons/png/512/888/888879.png',
+    )
+
+  const commandFiles = await fs.promises.readdir('./commands/', null)
+
+  for (const file of commandFiles) {
+    const element_in_folder = fs.statSync(`./commands/${file}`)
+    if (element_in_folder.isDirectory()) {
+      // eslint-disable-next-line no-await-in-loop
+      await addCommandsFromSubFolder(commandsEmbed, file)
     }
-  })
+  }
+  return commandsEmbed
+}
 
-  async function getCommands() {
-    const commandsEmbed = new discord.MessageEmbed()
-      .setColor('#ffa500')
-      .setAuthor({ name: 'Help', iconURL: 'https://bit.ly/3CJU0lf' })
-      .setTimestamp()
-      .setFooter(
-        `[ID] ${config.ids.userID.botUserID} \nstarted`,
-        'https://image.flaticon.com/icons/png/512/888/888879.png',
-      )
-
-    // Get all enteties from folder
-    const commandFiles = await fs.promises.readdir('./commands/', null)
-
-    for (const file of commandFiles) {
-      const element_in_folder = fs.statSync(`./commands/${file}`)
-      if (element_in_folder.isDirectory()) {
-        // Check if element in folder is a subfolder
-        // eslint-disable-next-line no-await-in-loop
-        await addCommandsFromSubFolder(commandsEmbed, file)
-      }
+async function addCommandsFromSubFolder(_commandsEmbed, file): Promise<void> {
+  const sub_directory = `./commands/${file}/`
+  try {
+    const files = await fs.promises.readdir(sub_directory)
+    let commandsInSubfolder = ' '
+    for (const command of files) {
+      const slicedFileName = command.split('.ts')[0]
+      commandsInSubfolder += `${slicedFileName}\n`
     }
-
-    // Return full object with all commands
-    return commandsEmbed
-
-    async function addCommandsFromSubFolder(_commandsEmbed, file) {
-      const sub_directory = `./commands/${file}/`
-      try {
-        const files = await fs.promises.readdir(sub_directory)
-        let commandsInSubfolder = ' '
-        for (const command of files) {
-          const slicedFileName = command.split('.ts')[0]
-          commandsInSubfolder += `${slicedFileName}\n`
-        }
-        _commandsEmbed.addFields({
-          name: file,
-          value: commandsInSubfolder,
-          inline: true,
-        })
-      } catch (e) {
-        console.log(e)
-      }
-    }
+    _commandsEmbed.addFields({
+      name: file,
+      value: commandsInSubfolder,
+      inline: true,
+    })
+  } catch (e) {
+    console.log(e)
   }
 }
