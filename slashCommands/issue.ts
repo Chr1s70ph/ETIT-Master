2ff4530051e2dc0cf252a73c88904ad7dee19547
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-25 13:00:13 +0100

    refactor issue

--- slashCommands/issue.ts
+++ slashCommands/issue.ts
@@ -3,6 +3,40 @@ const createIssue = require('github-create-issue')
 const REPOSITORY = 'Chr1s70ph/ETIT-Master'
 
 exports.run = async client => {
+  await postSlashCommand(client)
+
+  client.on('interactionCreate', async (interaction: Interaction) => {
+    if (!interaction.isCommand()) return
+    const COMMAND = interaction.commandName
+    if (COMMAND !== 'vorschlag') return
+    await respond(interaction, COMMAND, client)
+  })
+}
+
+async function respond(interaction, COMMAND: string, client: any) {
+  console.log(`User ${interaction.user.username} issued /${COMMAND}`)
+
+  const options = {
+    token: client.config.github_token,
+    body: interaction.options.getString('vorschlag'),
+  }
+
+  createIssue(
+    REPOSITORY,
+    `${interaction.user.username}'s Vorschlag${
+      interaction.options.getString('titel') ? `: ${interaction.options.getString('titel')}` : ''
+    }`,
+    options,
+    clbk,
+  )
+
+  await interaction.reply({
+    embeds: [new MessageEmbed().setTitle('Vorschlag angekommen!').setDescription('Vielen Dank für deinen Vorschlag!')],
+    ephemeral: true,
+  })
+}
+
+async function postSlashCommand(client: any) {
   await client.api.applications(client.user.id).commands.post({
     data: {
       name: 'vorschlag',
@@ -22,35 +56,6 @@ exports.run = async client => {
         },
       ],
     },
-  })
-
-  client.on('interactionCreate', async (interaction: Interaction) => {
-    // Type 2 interactions are slashcommands
-    if (!interaction.isCommand()) return
-    const COMMAND = interaction.commandName
-    if (COMMAND !== 'vorschlag') return
-    console.log(`User ${interaction.user.username} issued /${COMMAND}`)
-
-    const options = {
-      token: client.config.github_token,
-      body: interaction.options.getString('vorschlag'),
-    }
-
-    createIssue(
-      REPOSITORY,
-      `${interaction.user.username}'s Vorschlag${
-        interaction.options.getString('titel') ? `: ${interaction.options.getString('titel')}` : ''
-      }`,
-      options,
-      clbk,
-    )
-
-    await interaction.reply({
-      embeds: [
-        new MessageEmbed().setTitle('Vorschlag angekommen!').setDescription('Vielen Dank für deinen Vorschlag!'),
-      ],
-      ephemeral: true,
-    })
   })
 }
 
