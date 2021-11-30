7c2b1513b3f3a55e10bc89695bdb0dfe3e6ca106
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-30 21:30:59 +0100

    add docs to restart

--- commands/admin/restart.ts
+++ commands/admin/restart.ts
@@ -10,6 +10,9 @@ exports.description = ''
 exports.usage = 'restart'
 
 exports.run = (client: DiscordClient, message: Message) => {
+  /**
+   * Check if user has the correct rights to execute the command
+   */
   if (!Object.values(client.config.ids.acceptedAdmins).includes(message.author.id)) {
     return client.reply(message, { content: 'You do not have the permissions to perform that command.' })
   }
@@ -18,6 +21,10 @@ exports.run = (client: DiscordClient, message: Message) => {
   return client.send(message, { content: '🤖Restarting...' })
 }
 
+/**
+ * Pm2 restart handler
+ * @description Connect to the pm2 interface and restarts the current process
+ */
 function pm2Handle(): void {
   pm2.connect(err => {
     if (err) {
