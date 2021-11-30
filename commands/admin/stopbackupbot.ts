95fc54291fa5e572710e3ab59cb0ff703303bac3
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-30 22:35:23 +0100

    add docs to stopbackupbot

--- commands/admin/stopbackupbot.ts
+++ commands/admin/stopbackupbot.ts
@@ -1,6 +1,8 @@
 import { Message } from 'discord.js'
 import { DiscordClient } from '../../types/customTypes'
-// Const required, otherwise pm2 throws error
+/**
+ * Const required, otherwise pm2 throws error
+ */
 const pm2 = require('pm2')
 
 exports.name = 'stopbackupbot'
@@ -10,6 +12,9 @@ exports.description = 'stops backup bot'
 exports.usage = 'stopbackupbot'
 
 exports.run = (client: DiscordClient, message: Message) => {
+  /**
+   * Check if user has the correct rights to execute the command
+   */
   if (!Object.values(client.config.ids.acceptedAdmins).includes(message.author.id)) {
     return client.reply(message, { content: 'You do not have the permissions to perform that command.' })
   }
@@ -18,6 +23,10 @@ exports.run = (client: DiscordClient, message: Message) => {
   return client.send(message, { content: 'Stopping Backup Bot...' })
 }
 
+/**
+ * Pm2 restart handler.
+ * @description Connect to the pm2 interface and stops the backup process.
+ */
 function pm2Handle(): void {
   pm2.connect(err => {
     if (err) {
