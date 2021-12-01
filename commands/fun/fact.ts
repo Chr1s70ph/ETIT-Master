ff637badcea89be4bfee20b35fccf2d0a0e80e56
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-01 21:09:50 +0100

    add docs to fact

--- commands/fun/fact.ts
+++ commands/fun/fact.ts
@@ -11,8 +11,21 @@ exports.description = 'Willst du Fakten? Dann bist du hier genau richtig.'
 exports.usage = 'fact'
 
 exports.run = (client: DiscordClient, message: Message) => {
+  /**
+   * Read data from facts file.
+   * @type {string}
+   */
   const DATA = readFileSync(FACTS_FILE, 'utf-8').split('\n')
+
+  /**
+   * Rrandomly selected fact from {@link DATA}.
+   * @type {string}
+   */
   const fact = DATA[Math.floor(Math.random() * DATA.length)]
+
+  /**
+   * Send back random fact.
+   */
   return client.send(message, {
     embeds: [
       new MessageEmbed()
