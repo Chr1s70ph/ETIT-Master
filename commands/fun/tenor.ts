c4da823e406ccb21ddd238001a7923f311a75392
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-01 21:59:34 +0100

    add docs to tenor

--- commands/fun/tenor.ts
+++ commands/fun/tenor.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed, Message, MessageMentions } from 'discord.js'
+import { MessageEmbed, Message, MessageMentions, UserMention, MessageEmbedFooter } from 'discord.js'
 import { DiscordClient } from '../../types/customTypes'
 
 exports.name = 'tenor'
@@ -10,52 +10,84 @@ exports.usage = `tenor <searchQuery>`
 exports.aliases = ['gif']
 
 exports.run = (client: DiscordClient, message: Message, args: string[]) => {
+  /**
+   * Get {@link UserMention} from message.
+   * @type {string}
+   */
   const userPing = args.find(value => MessageMentions.USERS_PATTERN.test(value))
 
-  args = removeMatching(args, MessageMentions.USERS_PATTERN)
+  /**
+   * Define search query.
+   */
+  const searchQuery = removeMatching(args, MessageMentions.USERS_PATTERN).join(' ')
 
-  let searchQuery = ''
-  for (const word in args) {
-    searchQuery = `${searchQuery + args[word]} `
-  }
-
-  if (searchQuery.length === 0) {
-    return client.send(message, { content: 'Bitte gebe einen suchwort an!' })
-  }
-
-  return queryTenorAndReply(client, searchQuery, message, userPing)
+  /**
+   * Send reply.
+   */
+  return searchQuery.length === 0
+    ? client.send(message, { content: 'Bitte gebe einen suchwort an!' })
+    : queryTenorAndReply(client, searchQuery, message, userPing)
 }
 
-// https://stackoverflow.com/a/3661083/10926046
+/**
+ * Derived from https://stackoverflow.com/a/3661083/10926046
+ * @param {string[]} originalArray Array to get string from.
+ * @param {RegExp} regex RegularExpression to look for.
+ * @returns {string} String extracted from Array.
+ */
 function removeMatching(originalArray: string[], regex: RegExp): string[] {
-  let j = 0
-  while (j < originalArray.length) {
-    if (regex.test(originalArray[j])) originalArray.slice(j, 1)
-    else j++
+  let iterator = 0
+  while (iterator < originalArray.length) {
+    if (regex.test(originalArray[iterator])) originalArray.slice(iterator, 1)
+    else iterator++
   }
   return originalArray
 }
 
+/**
+ * Querys the Tenor-API and sends a reply with a random GIF.
+ * @param {DiscordClient} client Bot-Client.
+ * @param {string} searchQuery Query to search for with the tenor api.
+ * @param {Message<boolean>} message Command-message to respond to.
+ * @param {string} userPing User to mention.
+ * @returns {any}
+ */
 function queryTenorAndReply(
   client: DiscordClient,
   searchQuery: string,
   message: Message<boolean>,
   userPing: string,
 ): any {
+  /**
+   * Create instance of the tenorjs wrapper.
+   */
   const Tenor = require('tenorjs').client(client.config.tenor)
 
+  /**
+   * Query the Tenor-API for one random GIF
+   */
   return Tenor.Search.Random(searchQuery, '1').then(Results => {
-    const embed = new MessageEmbed()
-      .setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
+    /**
+     * New {@link MessageEmbed} with random color.
+     *
+     * {@link MessageEmbedFooter} is set to the user's tag and avatar who issued the command.
+     */
+    let embed = new MessageEmbed()
       .setColor('RANDOM')
-    if (Results.length === 0) {
-      embed.setDescription(`<@${message.author.id}> Es konnten keine Gifs gefunden werden für: '${searchQuery}'`)
-      return client.send(message, { embeds: [embed] })
-    }
-    Results.forEach(Post => {
-      const gifUrl = Post.media.find(element => Object.prototype.hasOwnProperty.call(element, 'gif')).gif.url
-      embed.setImage(gifUrl)
-    })
+      .setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
+
+    /**
+     * Set image of embed if Tenor query returned any.
+     * If no results have been returned, set Description to tell user that nothing has been found.
+     */
+    embed =
+      Results.length === 0
+        ? embed.setDescription(`<@${message.author.id}> Es konnten keine Gifs gefunden werden für: '${searchQuery}'`)
+        : embed.setImage(Results[0].media.find(element => Object.prototype.hasOwnProperty.call(element, 'gif')).gif.url)
+
+    /**
+     * Send back {@link embed} with {@link userPing}
+     */
     return client.send(message, {
       content: userPing,
       embeds: [embed],
