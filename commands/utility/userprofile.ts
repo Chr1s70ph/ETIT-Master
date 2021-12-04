bc6c24553a58e59885e832fcc270b049ebf6bd7f
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-04 18:18:02 +0100

    add docs to userprofile

--- commands/utility/userprofile.ts
+++ commands/utility/userprofile.ts
@@ -11,15 +11,25 @@ exports.usage = 'userprofile {optional: @mentionedUser}'
 exports.aliases = ['profile']
 
 exports.run = async (client: DiscordClient, message: Message) => {
+  /**
+   * Set the user to fetch information about.
+   */
   const user = (
     message.mentions.members.first() !== undefined ? message.mentions.members.first() : message.author
   ) as GuildMember
+
+  /**
+   * Fetch information about {@link user}.
+   */
   const { forceFetchedUser, messageUser, userJoinedTimestamp, userPremiumSinceTimestamp } = await getUserInfo(
     message,
     user,
     client,
   )
 
+  /**
+   * Send reply with information about the user.
+   */
   return client.reply(message, {
     embeds: [
       new MessageEmbed()
@@ -39,6 +49,13 @@ exports.run = async (client: DiscordClient, message: Message) => {
   })
 }
 
+/**
+ *
+ * @param {Message} message Message sent by the user
+ * @param {GuildMember} user User to fetch information about
+ * @param {DiscordClient} client Bot-Client
+ * @returns {{User, GuildMember, string, string}}
+ */
 async function getUserInfo(
   message: Message<boolean>,
   user: GuildMember,
@@ -49,16 +66,32 @@ async function getUserInfo(
   userJoinedTimestamp: string
   userPremiumSinceTimestamp: string
 }> {
+  /**
+   * Find user.
+   */
   const messageUser = message.guild.members.cache.find(foundUser => foundUser.id === user.id) as GuildMember
 
+  /**
+   * Force fetch user to get banner url.
+   */
   const forceFetchedUser = (await client.users.fetch(messageUser, { force: true })) as User
 
+  /**
+   * Fetch timestamp when user joined server.
+   */
   const userJoinedTimestamp = messageUser.joinedTimestamp
     .toString()
     .substring(0, messageUser.joinedTimestamp.toString().length - 3)
 
+  /**
+   * Fetch timestamp since when user has nitro subsription.
+   */
   const userPremiumSinceTimestamp = messageUser.premiumSinceTimestamp
     ?.toString()
     .substring(0, messageUser.premiumSinceTimestamp?.toString().length - 3)
+
+  /**
+   * Return information
+   */
   return { forceFetchedUser, messageUser, userJoinedTimestamp, userPremiumSinceTimestamp }
 }
