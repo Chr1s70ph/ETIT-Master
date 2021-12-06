26f9e82d8de85a8257df94d5fd8c9545b264e2c3
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-06 12:44:37 +0100

    add docs to ready

--- events/ready.ts
+++ events/ready.ts
@@ -2,7 +2,18 @@ import { loadScripts, loadSlashCommands } from '../index'
 import { DiscordClient } from '../types/customTypes'
 
 exports.run = async (client: DiscordClient) => {
+  /**
+   * Load all scripts.
+   */
   await loadScripts(client)
+
+  /**
+   * Load all slashCommands.
+   */
   await loadSlashCommands(client)
+
+  /**
+   * Log that the bot is online and operational.
+   */
   console.log('Online!')
 }
