0b7b9379129d6673041a870fff0d53b8298454da
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-26 18:51:31 +0100

    change key

--- commands/tests/hello.ts
+++ commands/tests/hello.ts
@@ -11,7 +11,7 @@ exports.usage = 'hello'
 exports.aliases = ['hallo']
 
 exports.run = (client: DiscordClient, message: Message, language: string) => {
-  const returnString = client.translate({ key: 'devtest.welcome', lng: language })
+  const returnString = client.translate({ key: 'commands.tests.hello.welcome', lng: language })
 
   return client.reply(message, { content: returnString })
 }
