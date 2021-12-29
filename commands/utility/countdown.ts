23314b4de931cb48a1ad4a30cbfa51b75b6d7fef
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-29 21:43:00 +0100

    add translation

--- commands/utility/countdown.ts
+++ commands/utility/countdown.ts
@@ -1,5 +1,5 @@
-import { Message, MessageEmbed } from 'discord.js'
-import { DiscordClient } from '../../types/customTypes'
+import { MessageEmbed } from 'discord.js'
+import { DiscordClient, DiscordMessage } from '../../types/customTypes'
 
 exports.name = 'countdown'
 
@@ -7,39 +7,41 @@ exports.description = 'Ein simpler countdown von 10 runter'
 
 exports.usage = 'countdown'
 
-exports.run = async (client: DiscordClient, message: Message) => {
+exports.run = async (client: DiscordClient, message: DiscordMessage) => {
   /**
    * Embed template.
    */
   const msgEmbed = new MessageEmbed()
-    .setTitle('Liftoff in:')
+    .setTitle(client.translate({ key: 'commands.utility.countdown.Countdown', lng: message.author.language }))
     .setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
     .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/3/33/Cartoon_space_rocket.png')
 
   /**
    * Reply with the embed and set the description.
    */
-  const msg = await message
+  const msg = (await message
     .reply({
       embeds: [msgEmbed.setDescription(`🚀10🚀`)],
     })
     .then(_msg => {
       message.delete()
       return _msg
-    })
+    })) as DiscordMessage
 
   /**
    * Update the embed to count down from 10.
    */
-  editEmbed(msg, msgEmbed)
+  editEmbed(msg, msgEmbed, client)
 }
+
 /**
  * Edit the embed to display a countdown.
- * @param {Message<boolean>} msg Message of the reply with the embed
+ * @param {DiscordMessage} msg Message of the reply with the embed
  * @param {MessageEmbed} msgEmbed The embed to edit and attatch
+ * @param {DiscordMessage} client Bot-Client
  * @returns {void}
  */
-function editEmbed(msg: Message<boolean>, msgEmbed: MessageEmbed): void {
+function editEmbed(msg: DiscordMessage, msgEmbed: MessageEmbed, client: DiscordClient): void {
   /**
    * Count only 8, 6, 4, 3, 2, 1, 0 and skips 9, 7, 5 due to API limits.
    */
@@ -52,8 +54,19 @@ function editEmbed(msg: Message<boolean>, msgEmbed: MessageEmbed): void {
       }
     } else {
       setTimeout(() => {
-        if (i === 0) msg.edit({ embeds: [msgEmbed.setTitle('Abfluuug').setDescription(`🚀Abgehoben🚀`)] })
-        else msg.edit({ embeds: [msgEmbed.setDescription(`🚀${i}🚀`)] })
+        if (i === 0) {
+          msg.edit({
+            embeds: [
+              msgEmbed
+                .setTitle('Abfluuug')
+                .setDescription(
+                  client.translate({ key: 'commands.utility.countdown.Liftoff', lng: msg.author.language }),
+                ),
+            ],
+          })
+        } else {
+          msg.edit({ embeds: [msgEmbed.setDescription(`🚀${i}🚀`)] })
+        }
       }, 1000 * (10 - i))
     }
   }
