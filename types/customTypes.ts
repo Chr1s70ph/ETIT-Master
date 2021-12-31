e34f1a6715134b14eacb47ff746b522f1a577730
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-31 14:30:40 +0100

    move eslint comment

--- types/customTypes.ts
+++ types/customTypes.ts
@@ -1,8 +1,8 @@
 import { readdirSync } from 'fs'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {
   Client,
   Message,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   TextChannel,
   MessageOptions,
   Collection,
