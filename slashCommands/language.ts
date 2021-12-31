0dc78c96256fc124df4c5eceef90f6370dfbe764
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-31 16:00:52 +0100

    add inital lanuage selector

---
+++ slashCommands/language.ts
@@ -0,0 +1,68 @@
+import { readdir } from 'fs/promises'
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { MessageActionRow, MessageSelectMenu } from 'discord.js'
+import { DiscordClient, DiscordInteraction } from '../types/customTypes'
+
+export const data = new SlashCommandBuilder().setName('language').setDescription('Choose your language')
+
+exports.respond = async (client: DiscordClient, interaction: DiscordInteraction) => {
+  /**
+   * Array containing names of all language files
+   */
+  const langFiles = await readdir('./locales/')
+
+  /**
+   * Otions for {@link MessageActionRow}
+   */
+  let options = []
+
+  /**
+   * Lopp through all languages
+   */
+  for (const file in langFiles) {
+    /**
+     * Language indicator
+     */
+    const lang = langFiles[file].split('.')[0]
+
+    /**
+     * Language fully written out
+     */
+    const languageName = client.translate({ key: 'language', lng: lang })
+
+    /**
+     * Option for {@link MessageActionRow}
+     */
+    const option = {
+      label: `${client.translate({ key: 'flag', lng: lang })} ${languageName}`,
+      description: languageName,
+      value: lang,
+    }
+
+    /**
+     * Add option to Array of all possible options
+     */
+    options = [...options, option]
+  }
+
+  /**
+   * Create new {@link MessageActionRow}
+   */
+  const row = new MessageActionRow().addComponents(
+    /**
+     * Add {@link MessageSelectMenu}
+     */
+    new MessageSelectMenu()
+      .setCustomId('languageSelector')
+      .setPlaceholder(client.translate({ key: 'slashCommands.language.DefaultSelect', lng: interaction.user.language }))
+      .addOptions(options),
+  )
+
+  /**
+   * Reply with {@link row} to interaction
+   */
+  await interaction.reply({
+    content: client.translate({ key: 'slashCommands.language.Select', lng: interaction.user.language }),
+    components: [row],
+  })
+}
