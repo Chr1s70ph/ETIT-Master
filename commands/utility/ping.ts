3c5662e176de5ad0dea471bdcff85f1edf57ab34
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-12-31 09:39:01 +0100

    multi lingual ping

--- commands/utility/ping.ts
+++ commands/utility/ping.ts
@@ -1,15 +1,21 @@
-import { Message } from 'discord.js'
-import { DiscordClient } from '../../types/customTypes'
+import { DiscordClient, DiscordMessage } from '../../types/customTypes'
 exports.name = 'ping'
 
 exports.description = 'pong'
 
 exports.usage = 'ping'
 
-exports.run = (client: DiscordClient, message: Message) =>
+exports.run = (client: DiscordClient, message: DiscordMessage) =>
   /**
    * Send reply with API latency.
    */
   client.send(message, {
-    content: `🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`,
+    content: client.translate({
+      key: 'commands.utility.ping',
+      options: {
+        latency: Date.now() - message.createdTimestamp,
+        api_latency: Math.round(client.ws.ping),
+        lng: message.author.language,
+      },
+    }),
   })
