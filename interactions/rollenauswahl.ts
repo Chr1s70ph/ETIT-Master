24d055657280dd47bf557bf92527cb9893d3dd0d
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2022-01-11 20:56:36 +0100

    use cost and implement switch between subcommands

--- interactions/rollenauswahl.ts
+++ interactions/rollenauswahl.ts
@@ -1,31 +1,58 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { DiscordClient, DiscordCommandInteraction } from '../types/customTypes'
 
+const DEGREE_COURSE = 'studiengang'
+const DEGREE_CHOICES: [string, string][] = [
+  ['ETIT Bachelor', 'etit-bachelor'],
+  ['ETIT Master', 'etit-master'],
+  ['MIT Bachelor', 'mit-bachelor'],
+  ['MIT Master', 'mit-master'],
+  ['KIT Bachelor', 'kit-bachelor'],
+  ['KIT Master', 'kit-master'],
+]
+const COURSES = 'fächer'
+const FREETIME = 'freizeit'
+
 export const data = new SlashCommandBuilder()
   .setName('rollenauswahl')
   .setDescription('Wähle deine Rollen')
   .addSubcommand(subcommand =>
     subcommand
-      .setName('studiengang')
+      .setName(DEGREE_COURSE)
       .setDescription('Wähle deinen Studiengang')
       .addStringOption(option =>
         option
           .setDescription('Wähle deinen Studiengang')
           .setRequired(true)
-          .setName('studiengang')
-          .setChoices([
-            ['ETIT Bachelor', 'etit-bachelor'],
-            ['ETIT Master', 'etit-master'],
-            ['MIT Bachelor', 'mit-bachelor'],
-            ['MIT Master', 'mit-master'],
-            ['KIT Bachelor', 'kit-bachelor'],
-            ['KIT Master', 'kit-master'],
-          ]),
+          .setName(DEGREE_COURSE)
+          .setChoices(DEGREE_CHOICES),
       ),
   )
-  .addSubcommand(subcommand => subcommand.setName('fächer').setDescription('Wähle deine Fächer'))
-  .addSubcommand(subcommand => subcommand.setName('freizeit').setDescription('Wähle deine Freizeit Rollen'))
+  .addSubcommand(subcommand => subcommand.setName(COURSES).setDescription('Wähle deine Fächer'))
+  .addSubcommand(subcommand => subcommand.setName(FREETIME).setDescription('Wähle deine Freizeit Rollen'))
 
 exports.Command = async (client: DiscordClient, interaction: DiscordCommandInteraction): Promise<void> => {
-  await interaction.reply({ content: `${interaction.options.data[0].value}` })
+  const subCommand = interaction.options.getSubcommand()
+  switch (subCommand) {
+    case DEGREE_COURSE:
+      degreeCourse()
+      break
+    case COURSES:
+      courses()
+      break
+    case FREETIME:
+      freetime()
+      break
+
+    default:
+      break
+  }
+  console.log(interaction)
+  await interaction.reply({ content: `${interaction.options.getSubcommand()}` })
 }
+
+function degreeCourse() {}
+
+function courses() {}
+
+function freetime() {}
