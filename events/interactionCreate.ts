a56b73747a04e19908c4f319d07a402104139bf6
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2022-01-17 11:49:38 +0100

    add user join and leave messages

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -3,9 +3,9 @@ import {
   DiscordButtonInteraction,
   DiscordClient,
   DiscordCommandInteraction,
-  DiscordMessageContextMenuInteraction,
+  DiscordMessageContextMenuCommandInteraction,
   DiscordSelectMenuInteraction,
-  DiscordUserContextMenuInteraction,
+  DiscordUserContextMenuCommandInteraction,
 } from '../types/customTypes'
 
 exports.run = (client: DiscordClient, interaction: any) => {
@@ -38,13 +38,13 @@ exports.run = (client: DiscordClient, interaction: any) => {
     const DiscordInteraction = interaction as DiscordCommandInteraction
     commandfile.Command(client, DiscordInteraction)
   } else if (interaction.isMessageContextMenu()) {
-    const DiscordInteraction = interaction as DiscordMessageContextMenuInteraction
+    const DiscordInteraction = interaction as DiscordMessageContextMenuCommandInteraction
     commandfile.MessageContextMenu(client, DiscordInteraction)
   } else if (interaction.isSelectMenu()) {
     const DiscordInteraction = interaction as DiscordSelectMenuInteraction
     commandfile.SelectMenu(client, DiscordInteraction)
   } else if (interaction.isUserContextMenu()) {
-    const DiscordInteraction = interaction as DiscordUserContextMenuInteraction
+    const DiscordInteraction = interaction as DiscordUserContextMenuCommandInteraction
     commandfile.UserContextMenu(client, DiscordInteraction)
   }
 }
