353790968f29fa9285c9313d3ceeb1dae714c88e
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2022-02-08 12:47:41 +0100

    fix guildMemberRemove

--- events/guildMemberRemove.ts
+++ events/guildMemberRemove.ts
@@ -22,14 +22,14 @@ exports.run = (client: DiscordClient, member: GuildMember) => {
    * Update membercounter.
    */
   channel.setName(`👥 ${memberCount.toLocaleString()} Mitglieder`)
-  console.log(`${member.user.username} joined. Updated membercount to ${memberCount.toLocaleString()}`)
+  console.log(`${member.user.username} left. Updated membercount to ${memberCount.toLocaleString()}`)
 }
 
 async function serverGoodByeMessage(client: DiscordClient, member: GuildMember) {
   const embed = new MessageEmbed()
     .setColor('#FF0000')
     .setTitle(`${member.user.username}#${member.user.discriminator}`)
-    .setDescription(`<@${member.user.id}> hat den Server!`)
+    .setDescription(`<@${member.user.id}> hat den Server verlassen!`)
     .addField('Server beigetreten am', member.joinedAt.toString(), false)
     .addField('Account erstellt am', member.user.createdAt.toString(), false)
     .setAuthor({ name: '😭 Mitglieder-Austritt' })
