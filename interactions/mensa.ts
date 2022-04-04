ac0281fe67856d5e9e9b0c22afdfd4caa88e368b
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2022-04-04 22:36:33 +0200

    add slashbuilder and fix lint

--- interactions/mensa.ts
+++ interactions/mensa.ts
@@ -1,91 +1,111 @@
-const Discord = require('discord.js')
-const embedHelper = require('../helper/embed.js')
-const fs = require('fs')
-const https = require('https')
-const id = require('../private/id.js')
-const loginData = require('../private/loginData.js')
-const mdHelper = require('../helper/md.js')
-const sendErrorMessageHelper = require('../helper/sendErrorMessage.js')
-const { settings } = require('../ETIT-Chef.js')
-const timestampHelper = require('../helper/timestamp.js')
-const url = require('../private/url.js')
+import * as fs from 'fs'
+import * as https from 'https'
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { MessageEmbed } from 'discord.js'
+import { DiscordClient, DiscordCommandInteraction } from '../types/customTypes'
 
-exports.name = 'mensa'
-
-exports.description = 'Dieser Befehl zeigt den Speiseplan einer Mensa an.'
-
-exports.usage = `${settings.prefix}mensa {MENSA} {TAG}`
-
-exports.group = 'info'
-
-exports.isSlashCommand = true
-
-exports.permissionLevel = 0
-
-exports.userPermissionBypass = []
-
-exports.slash_data = {
-  name: 'mensa',
-  description: 'Dieser Befehl zeigt den Speiseplan einer Mensa an.',
-  options: [
-    {
-      name: 'wochentag',
-      description: 'Der Wochentag, der angezeigt werden soll.',
-      type: 3,
-      required: false,
-      choices: [
-        {
-          name: 'Montag',
-          value: 'mo',
-        },
-        {
-          name: 'Dienstag',
-          value: 'di',
-        },
-        {
-          name: 'Mittwoch',
-          value: 'mi',
-        },
-        {
-          name: 'Donnerstag',
-          value: 'do',
-        },
-        {
-          name: 'Freitag',
-          value: 'fr',
-        },
-      ],
-    },
-    {
-      name: 'mensa',
-      description: 'Die Mensa, die angezeigt werden soll.',
-      type: 3,
-      required: false,
-      choices: [
-        {
-          name: 'Am Adenauerring',
-          value: 'adenauerring',
-        },
-        {
-          name: 'Erzbergstraße',
-          value: 'erzberger',
-        },
-        {
-          name: 'Schloss Gottesaue',
-          value: 'gottesaue',
-        },
-        {
-          name: 'Tiefbronner Straße',
-          value: 'tiefenbronner',
-        },
-        {
-          name: 'Caféteria Moltkestraße 30',
-          value: 'x1moltkestrasse',
-        },
-      ],
-    },
-  ],
+const weekdays = {
+  montag: {
+    name: 'montag',
+    value: 'mo',
+  },
+  dienstag: {
+    name: 'dienstag',
+    value: 'di',
+  },
+  mittwoch: {
+    name: 'mittwoch',
+    value: 'mi',
+  },
+  donnerstag: {
+    name: 'donnerstag',
+    value: 'do',
+  },
+  freitag: {
+    name: 'freitag',
+    value: 'fr',
+  },
 }
+
+const weekday_choices: [string, string][] = new Array([null, null])
+
+/**
+ * Add choices to Array
+ */
+for (const entry in weekdays) {
+  const choice: [string, string] = [entry, entry]
+  weekday_choices.push(choice)
+}
+weekday_choices.shift()
+
+const lines = {
+  adenauer: {
+    name: 'am adenauerring',
+    value: 'adenauerring',
+  },
+  erzberg: {
+    name: 'erzbergstraße',
+    value: 'erzberger',
+  },
+  schloss: {
+    name: 'achloss gottesaue',
+    value: 'gottesaue',
+  },
+  tiefbronner: {
+    name: 'tiefbronner straße',
+    value: 'tiefenbronner',
+  },
+  cafetaria: {
+    name: 'caféteria moltkestraße 30',
+    value: 'x1moltkestrasse',
+  },
+}
+
+const line_choices: [string, string][] = new Array([null, null])
+
+/**
+ * Add choices to Array
+ */
+for (const entry in lines) {
+  const choice: [string, string] = [entry, entry]
+  line_choices.push(choice)
+}
+line_choices.shift()
+
+export const data = new SlashCommandBuilder()
+  .setName('mensa')
+  .setDescription('Was es wohl heute zu Essen gibt?')
+  .addStringOption(option =>
+    option
+      .setName('wochentag')
+      .setDescription('Der Wochentag, der angezeigt werden soll.')
+      .addChoices(weekday_choices)
+      .setRequired(true),
+  )
+  .addStringOption(option =>
+    option
+      .setName('linie')
+      .setDescription('Die Mensa, die angezeigt werden soll.')
+      .addChoices(line_choices)
+      .setRequired(true),
+  )
+
+exports.Command = async (client: DiscordClient, interaction: DiscordCommandInteraction): Promise<void> => {
+  await interaction.reply({
+    content: 'Work in Progress',
+    embeds: [
+      new MessageEmbed()
+        .setAuthor({ name: 'Shush' })
+        .setDescription('Feddes schnidsel mit Rahmensoße und grünen Erbsen'),
+    ],
+  })
+}
+
+/**
+ * TODO:
+ * - Fix classes
+ * - Understand Buffer things in @link{_updateJson}
+ */
 
 class FoodLine {
   constructor(pName, pValue) {
@@ -158,24 +178,24 @@ const weekdayOptions = {
 }
 
 async function _updateJson(pClient, pMessage) {
-  let channel = pMessage ? pMessage.channel : pClient.channels.cache.get(id.channelId.BOT_TEST_LOBBY)
+  const channel = pMessage ? pMessage.channel : pClient.channels.cache.get(id.channelId.BOT_TEST_LOBBY)
 
-  let options = {
+  const options = {
     host: url.MENSA.API_HOST,
     port: 443,
     path: url.MENSA.API_PATH,
     headers: {
-      Authorization: 'Basic ' + new Buffer.from(loginData.MENSA.USER + ':' + loginData.MENSA.PWD).toString('base64'),
+      Authorization: `Basic ${new Buffer.from(`${loginData.MENSA.USER}:${loginData.MENSA.PWD}`).toString('base64')}`,
     },
   }
 
-  await https.get(options, function (res) {
+  await https.get(options, res => {
     let body = ''
-    res.on('data', function (data) {
+    res.on('data', data => {
       body += data
     })
-    res.on('end', function () {
-      fs.writeFile(settings.path + 'private/cache/mensa.txt', body, { flag: 'w+' }, err => {
+    res.on('end', () => {
+      fs.writeFile(`${settings.path}private/cache/mensa.txt`, body, { flag: 'w+' }, err => {
         if (err) {
           sendErrorMessageHelper.sendErrorMessageToChannel(
             pClient,
@@ -187,7 +207,7 @@ async function _updateJson(pClient, pMessage) {
         }
       })
     })
-    res.on('error', function (e) {
+    res.on('error', e => {
       sendErrorMessageHelper.sendErrorMessageToChannel(
         pClient,
         channel,
@@ -201,11 +221,11 @@ async function _updateJson(pClient, pMessage) {
 }
 
 async function _loadJSON() {
-  let data = await fs.promises.readFile(settings.path + 'private/cache/mensa.txt')
+  const data = await fs.promises.readFile(`${settings.path}private/cache/mensa.txt`)
   return JSON.parse(data)
 }
 
-async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
+async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa): Promise<MessageEmbed> {
   const embed = embedHelper.constructDefaultEmbed(pClient).setColor('#FAD51B').setAuthor('🍽️ Mensaplan')
 
   let jsonData = await _loadJSON()
@@ -213,7 +233,7 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
   let requestedWeekdayIndex = null
   let requestedDifference = null
 
-  let currentWeekday = new Date().getDay() - 1
+  const currentWeekday = new Date().getDay() - 1
 
   if (!pRequestedWeekday) {
     if (currentWeekday > 4) {
@@ -221,9 +241,9 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
       requestedWeekdayIndex = 0
       requestedDifference = 0
     } else {
-      let modifyDate = new Date().getHours() >= 15 ? 1 : 0
-      for (let weekday in weekdayOptions) {
-        if (weekdayOptions[weekday].index == currentWeekday + modifyDate) {
+      const modifyDate = new Date().getHours() >= 15 ? 1 : 0
+      for (const weekday in weekdayOptions) {
+        if (weekdayOptions[weekday].index === currentWeekday + modifyDate) {
           pRequestedWeekday = weekday
           requestedWeekdayIndex = currentWeekday + modifyDate
           requestedDifference = modifyDate
@@ -239,8 +259,11 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
     }
   }
 
-  currentDate = Math.round(Date.now() / 1000) // Take away the milliseconds
-  lastDate = Object.keys(jsonData['adenauerring'])[Object.keys(jsonData['adenauerring']).length - 1]
+  /**
+   * Date without milliseconds.
+   */
+  const currentDate = Math.round(Date.now() / 1000)
+  const lastDate: number = +Object.keys(jsonData.adenauerring)[Object.keys(jsonData.adenauerring).length - 1]
 
   if (currentDate + 7 * 86400 > lastDate) {
     // 7 * 86400 : number of seconds in one week
@@ -257,59 +280,61 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
     jsonData = await _loadJSON()
   }
 
-  if (Object.keys(jsonData).indexOf(pRequestedMensa) == -1) {
+  if (Object.keys(jsonData).indexOf(pRequestedMensa) === -1) {
     embed
-      .setTitle('Mensa ' + mensaOptions[pRequestedMensa]['name'])
+      .setTitle(`Mensa ${mensaOptions[pRequestedMensa].name}`)
       .setDescription('Diese Mensa hat am angeforderten Tag leider geschlossen.')
 
     pMessage.channel.send({
       embeds: [embed],
     })
 
-    return
+    return null
   }
 
-  for (let timestampKey in Object.keys(jsonData[pRequestedMensa])) {
-    let timestamp = Object.keys(jsonData[pRequestedMensa])[timestampKey]
+  for (const timestampKey in Object.keys(jsonData[pRequestedMensa])) {
+    const timestamp: number = +Object.keys(jsonData[pRequestedMensa])[timestampKey]
 
     if (timestamp > currentDate - 86400 + 86400 * requestedDifference) {
       // # 86400 number of seconds in one day
 
-      let date = new Date(timestamp * 1000)
+      const date = new Date(timestamp * 1000)
       date.setDate(date.getDate() + 1)
 
       embed
-        .setTitle('Mensa ' + mensaOptions[pRequestedMensa]['name'])
+        .setTitle(`Mensa ${mensaOptions[pRequestedMensa].name}`)
         .setDescription(
           `${date.toLocaleDateString('de-DE', { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' })}`,
         )
 
-      for (let foodLineIndex in mensaOptions[pRequestedMensa]['foodLines']) {
-        let foodLine = mensaOptions[pRequestedMensa]['foodLines'][foodLineIndex].name
+      for (const foodLineIndex in mensaOptions[pRequestedMensa].foodLines) {
+        const foodLine = mensaOptions[pRequestedMensa].foodLines[foodLineIndex].name
         let mealValues = ''
 
-        for (let foodLineDataIndex in jsonData[pRequestedMensa][timestamp][foodLine]) {
-          let foodLineData = jsonData[pRequestedMensa][timestamp][foodLine][foodLineDataIndex]
+        for (const foodLineDataIndex in jsonData[pRequestedMensa][timestamp][foodLine]) {
+          const foodLineData = jsonData[pRequestedMensa][timestamp][foodLine][foodLineDataIndex]
 
-          if (foodLineData['nodata']) {
+          // eslint-disable-next-line max-depth
+          if (foodLineData.nodata) {
             mealValues = '__Leider gibt es für diesen Tag hier keine Informationen!__'
             break
           }
 
-          if (foodLineData['closing_start']) {
-            mealValues = `__Leider ist hier heute geschlossen. Grund: ${foodLineData['closing_text']}__`
+          // eslint-disable-next-line max-depth
+          if (foodLineData.closing_start) {
+            mealValues = `__Leider ist hier heute geschlossen. Grund: ${foodLineData.closing_text}__`
             break
           }
 
-          let price = ` (${foodLineData['price_1'] == 0 ? '0.00' : foodLineData['price_1'].toFixed(2)}€)`
-          let meal = `__${foodLineData['meal']} ${price}__\n`
-          let dish = foodLineData['dish']
+          const price = ` (${foodLineData.price_1 === 0 ? '0.00' : foodLineData.price_1.toFixed(2)}€)`
+          const meal = `__${foodLineData.meal} ${price}__\n`
+          const dish = foodLineData.dish
 
-          mealValues += ['', '.'].indexOf(dish) == -1 ? `${meal}${dish}\n` : meal
+          mealValues += ['', '.'].indexOf(dish) === -1 ? `${meal}${dish}\n` : meal
 
-          let allAdditions = foodLineData['add'].join(', ')
+          const allAdditions = foodLineData.add.join(', ')
 
-          mealValues += allAdditions != '' ? `_Zusatz: [${allAdditions}]_` : '_Keine Zusätze_'
+          mealValues += allAdditions !== '' ? `_Zusatz: [${allAdditions}]_` : '_Keine Zusätze_'
 
           const foodContainsStringToEmoji = {
             bio: ':earth_africa:',
@@ -323,9 +348,11 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
             mensa_vit: 'Mensa Vital',
           }
 
-          for ([foodContainsKey, foodContainsVal] of Object.entries(foodContainsStringToEmoji)) {
+          // eslint-disable-next-line max-depth
+          for (const [foodContainsKey, foodContainsVal] of Object.entries(foodContainsStringToEmoji)) {
+            // eslint-disable-next-line max-depth
             if (foodLineData[foodContainsKey]) {
-              mealValues += ' ' + foodContainsVal
+              mealValues += ` ${foodContainsVal}`
             }
           }
 
@@ -334,8 +361,8 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
 
         if (mealValues) {
           embed.addFields({
-            name: `⠀\n:arrow_forward: ${mensaOptions[pRequestedMensa]['foodLines'][foodLineIndex].value} :arrow_backward:`,
-            value: mealValues + '\n',
+            name: `⠀\n:arrow_forward: ${mensaOptions[pRequestedMensa].foodLines[foodLineIndex].value} :arrow_backward:`,
+            value: `${mealValues}\n`,
             inline: true,
           })
         }
@@ -354,7 +381,7 @@ async function mensa(pClient, pMessage, pRequestedWeekday, pRequestedMensa) {
 }
 
 async function daily_mensa(pClient) {
-  let msg = new Discord.Message(pClient, {
+  const msg = new Discord.Message(pClient, {
     channel_id: id.channelId.MENSA,
     guild_id: id.serverId.ETIT_KIT,
     id: '123456789101112',
@@ -364,7 +391,7 @@ async function daily_mensa(pClient) {
   })
 
   mensa_switcher(pClient, msg)
-  setInterval(function () {
+  setInterval(() => {
     mensa_switcher(pClient, msg)
   }, 86400000) // 86400000 milliseconds are in a day
 }
@@ -375,13 +402,13 @@ async function mensa_switcher(pClient, pMessageOrInteraction) {
   if (pMessageOrInteraction instanceof Discord.Message) {
     let requestedWeekday = null
     let requestedMensa = 'adenauerring'
-    let params = pMessageOrInteraction.content.split(' ').map(elem => elem.toLowerCase())
+    const params = pMessageOrInteraction.content.split(' ').map(elem => elem.toLowerCase())
 
-    for (let weekday in weekdayOptions) {
+    for (const weekday in weekdayOptions) {
       if (params.indexOf(weekday) != -1) {
         requestedWeekday = weekday
-        requestedWeekdayIndex = weekdayOptions[weekday].index
-        if (requestedWeekdayIndex > weekdayOptions['fr'].index) {
+        const requestedWeekdayIndex = weekdayOptions[weekday].index
+        if (requestedWeekdayIndex > weekdayOptions.fr.index) {
           sendErrorMessageHelper.sendErrorMessage(
             pClient,
             pMessageOrInteraction,
@@ -393,13 +420,13 @@ async function mensa_switcher(pClient, pMessageOrInteraction) {
       }
     }
 
-    for (let mensaKey in mensaOptions) {
-      if (params.indexOf(mensaKey) != -1) {
+    for (const mensaKey in mensaOptions) {
+      if (params.indexOf(mensaKey) !== -1) {
         requestedMensa = mensaKey
       }
     }
 
-    let embed = await mensa(pClient, pMessageOrInteraction, requestedWeekday, requestedMensa)
+    const embed = await mensa(pClient, pMessageOrInteraction, requestedWeekday, requestedMensa)
 
     msg = await pMessageOrInteraction.channel.send({
       embeds: [embed],
@@ -409,14 +436,14 @@ async function mensa_switcher(pClient, pMessageOrInteraction) {
     let requestedMensa = 'adenauerring'
     switch (pMessageOrInteraction.options._hoistedOptions.length) {
       case 1:
-        if (pMessageOrInteraction.options.data[0].name == 'wochentag') {
+        if (pMessageOrInteraction.options.data[0].name === 'wochentag') {
           requestedWeekday = pMessageOrInteraction.options.data[0].value
         } else {
           requestedMensa = pMessageOrInteraction.options.data[0].value
         }
         break
       case 2:
-        if (pMessageOrInteraction.options.data[0].name == 'wochentag') {
+        if (pMessageOrInteraction.options.data[0].name === 'wochentag') {
           requestedWeekday = pMessageOrInteraction.options.data[0].value
           requestedMensa = pMessageOrInteraction.options.data[1].value
         } else {
@@ -426,7 +453,7 @@ async function mensa_switcher(pClient, pMessageOrInteraction) {
         break
     }
 
-    let embed = await mensa(pClient, pMessageOrInteraction, requestedWeekday, requestedMensa)
+    const embed = await mensa(pClient, pMessageOrInteraction, requestedWeekday, requestedMensa)
 
     msg = await pMessageOrInteraction.reply({
       embeds: [embed],
@@ -434,7 +461,7 @@ async function mensa_switcher(pClient, pMessageOrInteraction) {
     })
   }
 
-  if (msg.channel.type == 'GUILD_NEWS') {
+  if (msg.channel.type === 'GUILD_NEWS') {
     console.log('news')
     await msg.crosspost()
   }
