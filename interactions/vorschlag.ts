a7384df5b730a44e5f871f36499c609003bf70af
Chr1s70ph <christoph.peter.bluem@gmail.com>
2022-05-20 08:19:02 +0200

    ADD: complete vorschlag

--- interactions/vorschlag.ts
+++ interactions/vorschlag.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { MessageActionRow, Modal, ModalActionRowComponent, TextInputComponent } from 'discord.js'
+import { MessageActionRow, MessageEmbed, Modal, ModalActionRowComponent, TextInputComponent } from 'discord.js'
 import { DiscordClient, DiscordCommandInteraction, DiscordModalSubmitInteraction } from '../types/customTypes'
 const createIssue = require('github-create-issue')
 const REPOSITORY = 'Chr1s70ph/ETIT-Master'
@@ -7,60 +7,78 @@ const REPOSITORY = 'Chr1s70ph/ETIT-Master'
 export const data = new SlashCommandBuilder().setName('vorschlag').setDescription('Schlage etwas vor!')
 
 exports.Command = async (client: DiscordClient, interaction: DiscordCommandInteraction): Promise<void> => {
-  // Input field for the tile
+  /**
+   * Input field for the tile
+   */
   const titleInput = new TextInputComponent()
     .setCustomId('titleInput')
     .setLabel('Watt schlägste vor Minium?')
     .setStyle('SHORT')
+    .setRequired(true)
   const firstActionRow = new MessageActionRow<ModalActionRowComponent>().setComponents(titleInput)
 
-  // Input field for the body
+  /**
+   * Input field for the body
+   */
   const bodyInput = new TextInputComponent()
     .setCustomId('bodyInput')
     .setLabel('Erzähl doch noch n bissl')
     .setStyle('PARAGRAPH')
+    .setRequired(true)
   const secondActionRow = new MessageActionRow<ModalActionRowComponent>().setComponents(bodyInput)
 
-  // Show modal
+  /**
+   * Show modal
+   */
   await interaction.showModal(
     new Modal().setTitle('Vorschlag').setCustomId('vorschlag').addComponents(firstActionRow, secondActionRow),
   )
 }
 
 exports.Modal = async (client: DiscordClient, interaction: DiscordModalSubmitInteraction) => {
-  // TODO use modal inputs instead of options
-  // const interactionOptions = interaction.options as CommandInteractionOptionResolver
-  // const options = {
-  //   token: client.config.github_token,
-  //   body: interactionOptions.getString('vorschlag'),
-  // }
-  // try {
-  //   createIssue(
-  //     REPOSITORY,
-  //     `${interaction.user.username}'s Vorschlag${
-  //       interactionOptions.getString('titel') ? `: ${interactionOptions.getString('titel')}` : ''
-  //     }`,
-  //     options,
-  //     clbk,
-  //   )
-  //   await interaction.reply({
-  //     embeds: [
-  //       new MessageEmbed()
-  //         .setTitle(client.translate({ key: 'interactions.issue.Recieved', lng: interaction.user.language }))
-  //         .setDescription(client.translate({ key: 'interactions.issue.Thanks', lng: interaction.user.language })),
-  //     ],
-  //     ephemeral: true,
-  //   })
-  // } catch (error) {
-  //   await interaction.reply({
-  //     embeds: [
-  //       new MessageEmbed()
-  //         .setTitle(client.translate({ key: 'interactions.issue.Error', lng: interaction.user.language }))
-  //         .setDescription(client.translate({ key: 'interactions.issue.TryAgain', lng: interaction.user.language })),
-  //     ],
-  //   })
-  //   throw new Error(error)
-  // }
+  /**
+   * Title of the new issue
+   */
+  const issue_title = interaction.fields.getTextInputValue('titleInput')
+
+  /**
+   * Body of the new issue
+   */
+  const issue_body = interaction.fields.getTextInputValue('bodyInput')
+
+  /**
+   * Options to send to the Github-API
+   */
+  const options = {
+    token: client.config.github_token,
+    body: issue_body,
+  }
+  try {
+    /**
+     * Create new issue with {@link issue_title} and {@link issue_body}
+     */
+    createIssue(REPOSITORY, `${interaction.user.username}'s Vorschlag ${issue_title}`, options, clbk)
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(client.translate({ key: 'interactions.issue.Recieved', lng: interaction.user.language }))
+          .setDescription(client.translate({ key: 'interactions.issue.Thanks', lng: interaction.user.language })),
+      ],
+      ephemeral: true,
+    })
+  } catch (error) {
+    /**
+     *  Error handling
+     */
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(client.translate({ key: 'interactions.issue.Error', lng: interaction.user.language }))
+          .setDescription(client.translate({ key: 'interactions.issue.TryAgain', lng: interaction.user.language })),
+      ],
+    })
+    throw new Error(error)
+  }
 }
 
 function clbk(error: Error, issue: object, info): void {
