41f1b5d514c0ae2d65c9da8b910b5e5b02716e81
Christoph <christoph.peter.bluem@gmail.com>
2023-02-10 22:51:33 +0100

    FIX: interactions count

--- scripts/loginMessage.ts
+++ scripts/loginMessage.ts
@@ -1,21 +1,19 @@
-import { readdir as readdir_promise } from 'fs/promises'
+import { PathLike, readdirSync, statSync } from 'fs'
 import { EmbedBuilder, TextChannel } from 'discord.js'
 import project from '../package.json'
 import { DiscordClient } from '../types/customTypes'
 const os = require('os')
 const newLocal = 'child_process'
+const path = require('path')
+
 const latest_commit = require(newLocal).execSync('git rev-parse HEAD').toString().substring(0, 7)
+const interactionFilesArray: Array<PathLike> = []
 
-exports.run = async (client: DiscordClient) => {
-  /**
-   * List of slashCommands.
-   */
-  const interactionCount = await readdir_promise('./interactions/')
-
+exports.run = (client: DiscordClient) => {
   /**
    * Login message {@link EmbedBuilder}.
    */
-  const loginMessage = createEmbed(client, interactionCount)
+  const loginMessage = createEmbed(client)
 
   /**
    * Channel to send {@link loginMessage} to.
@@ -29,13 +27,13 @@ exports.run = async (client: DiscordClient) => {
    */
   channel.send({ embeds: [loginMessage] })
 }
+
 /**
  * Create loginmessage.
  * @param {DiscordClient} client Bot-Client
- * @param {string[]} slashCount All slashCommands
  * @returns {EmbedBuilder}
  */
-function createEmbed(client: DiscordClient, slashCount: string[]): EmbedBuilder {
+function createEmbed(client: DiscordClient): EmbedBuilder {
   return new EmbedBuilder()
     .setColor('#ffa500')
     .setAuthor({ name: client.user.tag, iconURL: client.user.avatarURL() })
@@ -60,7 +58,7 @@ function createEmbed(client: DiscordClient, slashCount: string[]): EmbedBuilder 
     .addFields(
       {
         name: 'Interactions geladen:',
-        value: slashCount.length.toString(),
+        value: getAllFiles('./interactions/', interactionFilesArray).length.toString(),
         inline: true,
       },
       {
@@ -74,4 +72,26 @@ function createEmbed(client: DiscordClient, slashCount: string[]): EmbedBuilder 
       text: `[ID] ${client.config.ids.userID.botUserID} \nstarted`,
       iconURL: client.guilds.cache.find(guild => guild.id === client.config.ids.serverID).iconURL(),
     })
+}
+
+/**
+ * Get number of files in directory including subdirectories
+ * @param {PathLike} dirPath Path
+ * @param {Array<PathLike>} arrayOfFiles Array of files in {@link dirPath}
+ * @returns {Array<PathLike>}
+ */
+function getAllFiles(dirPath: PathLike, arrayOfFiles: any[]) {
+  const files = readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(file => {
+    if (statSync(`${dirPath}/${file}`).isDirectory()) {
+      arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, '/', file))
+    }
+  })
+
+  return arrayOfFiles
 }
