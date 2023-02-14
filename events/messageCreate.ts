1f079b1d87909cca64bfd3e9fd4aa12ee1a8776a
Christoph <christoph.peter.bluem@gmail.com>
2023-02-14 22:31:49 +0100

    ADD: spell check

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,4 +1,5 @@
 import { EmbedBuilder } from 'discord.js'
+import textgears from 'textgears-api'
 import { DiscordClient, DiscordMessage } from '../types/customTypes'
 
 exports.run = (client: DiscordClient, message: DiscordMessage) => {
@@ -6,6 +7,11 @@ exports.run = (client: DiscordClient, message: DiscordMessage) => {
    * Only respond to messages sent by users.
    */
   if (message.author.bot) return
+
+  /**
+   * Annoy people :)
+   */
+  if (message.author.id === client.config.ids.userID.david) check_spelling(client, message)
 
   /**
    * DM handling and forwarding.
@@ -49,4 +55,30 @@ exports.run = (client: DiscordClient, message: DiscordMessage) => {
       throw new Error(error)
     }
   }
+}
+
+/**
+ * Just a small function to annoy a friend ;)
+ * @param {Discordlient} client Discord-Client
+ * @param {DiscordMessage} message Message to scan
+ */
+function check_spelling(client: DiscordClient, message: DiscordMessage) {
+  const test_query = message.toString()
+
+  const textgearsApi = textgears(client.config.textgears_api_key, { language: 'de-DE', ai: false })
+  textgearsApi
+    .checkGrammar(test_query)
+    .then(async return_data => {
+      console.log(return_data.response.errors.length)
+      if (return_data.response.errors.length > 0) {
+        await message.reply('FEHLER ENTDECKT!!')
+        for (const error of return_data.response.errors) {
+          // eslint-disable-next-line no-await-in-loop
+          await message.reply(`Error: ${error.bad}. Suggestions: ${error.better.join(', ')}`)
+        }
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
