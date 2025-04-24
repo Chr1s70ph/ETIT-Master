9684e9af8c78f6303acee07491064d6fd3503510
Christoph <christoph.peter.bluem@gmail.com>
2025-04-24 12:39:08 +0200

    adds shebang, executable and add watch parameter

--- run-pm2.sh
+++ run-pm2.sh
@@ -1,1 +1,3 @@
-pm2 start ts-node --name ETIT-Master --time --watch --restart-delay 5000 -- -P tsconfig.json index.ts
+#/bin/env sh
+
+pm2 start ts-node --name ETIT-Master --watch --time --restart-delay 5000 -- -P tsconfig.json index.ts
